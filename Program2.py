89fa827d568e4de15842a5b4d771f5a2a1ee21e6
Sarabeshwaran <113020922+Sarabeshwaran@users.noreply.github.com>
2023-08-25 12:06:54 +0530

    Add files via upload

---
+++ Program2.py
@@ -0,0 +1,8 @@
+import cv2
+image_path = r'C:\Users\welcome\Downloads\wallpaperflare.com_wallpaper.jpg'
+image = cv2.imread(image_path)
+blurred_image = cv2.GaussianBlur(image, (5, 5), 0)
+cv2.imshow('Original Image', image)
+cv2.imshow('Blurred Image', blurred_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()	
